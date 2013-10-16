6a5888494f5a0987cde324f3e30e9a7d81738a3c
VuPhucTho <vuphuctho93@gmail.com>
2013-10-17 00:19:12 +0800

    add ColorHistogram and TextureHistogram

---
+++ src/ColorHistogram.java
@@ -0,0 +1,8 @@
+/*
+ * Job of color Histogram
+ * Index image into histogram using color
+ * with CCV (Color Coherent Vector) 
+ */
+public class ColorHistogram {
+
+}
---
+++ src/TextureHistogram.java
@@ -0,0 +1,8 @@
+/*
+ * Job of class Texture Histogram
+ * Index image into histogram using texture
+ * with edge direction histogram techinique
+ */
+public class TextureHistogram {
+
+}
